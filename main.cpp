cf747eeadb4522a313fd84d11360c72a40b2da99
朱海文 <1032671220@qq.com>
2020-05-29 11:32:56 +0800

    应用图标，尝试

--- main.cpp
+++ main.cpp
@@ -3,27 +3,26 @@
 #include <QApplication>
 //导入串口包
 #include <QCoreApplication>
-#include <QList>
 #include <stdio.h>
 #include <QDebug>
-#include <QtSerialPort/QSerialPort>
-#include <QtSerialPort/QSerialPortInfo>
 
-//创建websocket
-#include <QWebSocketServer>
 
+#include <QIcon>
 #include "connector.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication qapplication(argc, argv);
+//    QString dir = QApplication::applicationDirPath();
+//    QApplication::addLibraryPath("./images");
+
+    qapplication.setWindowIcon(QIcon("gangtiexia.icns"));
 //    MainWindow w;
 //    w.show();
     qDebug()<< "main.cpp执行了";
 
     new Connector();
-    //InspectionServer server(1234,serial);
 
-    return a.exec();
+    return qapplication.exec();
 }
 
