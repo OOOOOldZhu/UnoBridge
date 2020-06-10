7715d6a5fb965a1880a35abfd6bd496aef6e8910
朱海文 <1032671220@qq.com>
2020-06-10 11:40:56 +0800

    a

--- main.cpp
+++ main.cpp
@@ -6,28 +6,9 @@
 #include <stdio.h>
 #include <QDebug>
 
-
 #include <QIcon>
 #include <QMessageBox>
 #include "connector.h"
-#include "SingleApplication.h"
-
-//int main(int argc, char *argv[])
-//{
-//    QSingleApplication app(argc,argv);
-//    if (app.isRunning())
-//    {
-//       QMessageBox::information(NULL, QStringLiteral("提示"), QStringLiteral("程序已运行！"));
-//       exit(0);
-//    }
-
-//    qDebug()<< "main.cpp执行了";
-//    app.setWindowIcon(QIcon("gangtiexia.icns"));
-//    new Connector();
-
-//    return app.exec();
-//}
-#include "MainWindow.h"
 #include "SingleApplication.h"
 
 int main(int argc, char *argv[]) {
@@ -36,6 +17,8 @@ int main(int argc, char *argv[]) {
        QMessageBox::information(NULL, QStringLiteral("提示"), QStringLiteral("程序已运行！"));
        return 0;
     }
-
+    qDebug()<< "main.cpp执行了";
+    app.setWindowIcon(QIcon("gangtiexia.icns"));
+    new Connector();
     return app.exec();
 }
